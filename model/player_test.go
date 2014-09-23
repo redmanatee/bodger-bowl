87583d6fb8971979478f1fff0b2097c7309dc3cf
Andrew Perkins <redmanatee@users.noreply.github.com>
2014-09-23 17:53:33 -0600

    Added a unit test for adding in csv values of players

--- model/player_test.go
+++ model/player_test.go
@@ -32,3 +32,43 @@ func TestPlayerSaveNoSeason(t *testing.T) {
 		t.Errorf("Expected player faction to be '%s' instead it was '%s'", faction, p.Faction)
 	}
 }
+
+func TestPlayerCsvSaveNilSeason(t *testing.T) {
+	c, err := aetest.NewContext(nil)
+    if err != nil {
+            t.Fatal(err)
+    }
+    defer c.Close()
+    csvString := `Player1,Skorne,player@somewhere.com,406-244-6613
+Player2,Circle,player2@somewhereelse.com,503-882-9933
+`
+	CreatePlayersFromCsv(c, nil, csvString)
+	p := LoadPlayer(c, nil, "player@somewhere.com")
+	if p == nil {
+		t.Fatal("Loading the first player gave us an unexpected nil player")
+	}
+	if p.Name != "Player1" {
+		t.Errorf("Expected player name to be '%s' not '%s'", "Player1", p.Name)
+	}
+	if p.Email != "player@somewhere.com" {
+		t.Errorf("Expected player email to be '%s' instead it was '%s'", "player@somewhere.com", p.Email)
+	}
+	if p.Faction != "Skorne" {
+		t.Errorf("Expected player faction to be '%s' instead it was '%s'", "Skorne", p.Faction)
+	}
+
+	p = LoadPlayer(c, nil, "player2@somewhereelse.com")
+	if p == nil {
+		t.Fatal("Loading the second player gave us an unexpected nil player")
+	}
+	if p.Name != "Player2" {
+		t.Errorf("Expected player name to be '%s' not '%s'", "Player2", p.Name)
+	}
+	if p.Email != "player2@somewhereelse.com" {
+		t.Errorf("Expected player email to be '%s' instead it was '%s'", "player2@somewhereelse.com", p.Email)
+	}
+	if p.Faction != "Circle" {
+		t.Errorf("Expected player faction to be '%s' instead it was '%s'", "Circle", p.Faction)
+	}
+
+}
