e03e6c7096a1a28ef2da39ffb6da2b67cc657846
Andrew Perkins <redmanatee@users.noreply.github.com>
2014-10-04 11:15:26 -0600

    Added a test for empty player lists being split into faction maps

--- src/admin/schedule_admin_test.go
+++ src/admin/schedule_admin_test.go
@@ -1,4 +1,14 @@
 package admin
 
 import (
+	"testing"
+	"model"
 )
+
+func TestPlayersByFactionEmptyList(t *testing.T) {
+	players := make([]model.Player, 0)
+	playersByFaction := playersByFaction(players)
+	if len(playersByFaction) != 0 {
+		t.Errorf("Expected an empty map of players by faction, instead had one of length '%i'", len(playersByFaction))
+	}
+}
