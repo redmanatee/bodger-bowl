1ed3fc467b94aae05a63b00a1fb64c14d75c5aa1
Andrew Perkins <redmanatee@users.noreply.github.com>
2014-10-04 10:00:11 -0600

    Added in the admin schedule code

---
+++ admin/schedule_admin.go
@@ -0,0 +1,14 @@
+package admin
+
+import (
+	"model"
+)
+
+func CreateSeasonSchedule(season *model.Season, conferenceCount int, divisionCount int, weeksCount int, playoffs int, players []model.Player) {
+
+}
+
+
+func CreateConferences(season *model.Season, conferenceCount int, divisionCount int, players []model.Player) {
+	
+}
