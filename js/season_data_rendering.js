aefbdc89fc496a0b7e950997ece04c17d21025f6
Andrew Perkins <redmanatee@users.noreply.github.com>
2014-10-20 20:07:00 -0600

    Updated some rendering for simple changes to make it easier to navigate for now

--- js/season_data_rendering.js
+++ js/season_data_rendering.js
@@ -4,14 +4,14 @@ var SeasonData = React.createClass({
 	render: function() {
 		var admin = window.location.pathname.indexOf("admin") > -1;
 		return (
+			<div>
 				<div className="row">
 					<div className="small-grid-12 columns">
 						<SeasonScheduleTable admin={admin} />
 					</div>
-					<div className="small-grid-12 columns">
-						<ConferenceContainer admin={admin} />
-					</div>
 				</div>
+				<ConferenceContainer admin={admin} />
+			</div>
 		);
 	}
 });
