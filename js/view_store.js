5c66ab180ff7889c0023c0dc8f1b369b371331fb
Dan Erikson <derikson3@gmail.com>
2014-12-07 08:46:58 -0700

    Refactored window.viewStore to move variables into the object.

--- js/view_store.js
+++ js/view_store.js
@@ -1,41 +1,38 @@
 // keeps track of what is being displayed for the main tab set, the player tab set, and the player select box
 (function() {
 	'use strict';
-
-	var state = {mainTab: 1, playerTab: 1, playerName: null};
-
-	var historyEnabled = window.history && window.history.pushState;
-	if(historyEnabled) {
-		window.onpopstate = window.appActions.popHistoryState;
-		window.history.replaceState(state, '');
-	}
-
 	window.viewStore = Reflux.createStore({
 		init: function() {
+			this.state = {mainTab: 1, playerTab: 1, playerName: null};
+			this.historyEnabled = window.history && window.history.pushState;
+			if(this.historyEnabled) {
+				window.onpopstate = window.appActions.popHistoryState;
+				window.history.replaceState(this.state, '');
+			}
 			this.listenTo(window.appActions.viewMainTab, this.viewMainTab);
 			this.listenTo(window.appActions.viewPlayerTab, this.viewPlayerTab);
 			this.listenTo(window.appActions.viewPlayer, this.viewPlayer);
 			this.listenTo(window.appActions.popHistoryState, this.popHistoryState);
 		},
 		viewMainTab: function(key) {
-			state.mainTab = key;
-			if(historyEnabled) window.history.pushState(state, '');
-			this.trigger(state);
+			this.state.mainTab = key;
+			if(this.historyEnabled) window.history.pushState(this.state, '');
+			this.trigger(this.state);
 		},
 		viewPlayerTab: function(key) {
-			state.playerTab = key;
-			if(historyEnabled) window.history.pushState(state, '');
-			this.trigger(state);
+			this.state.playerTab = key;
+			if(this.historyEnabled) window.history.pushState(this.state, '');
+			this.trigger(this.state);
 		},
 		viewPlayer: function(playerName) {
-			state.playerName = playerName;
-			state.mainTab = 3;
-			if(historyEnabled) window.history.pushState(state, '');
-			this.trigger(state);
+			this.state.playerName = playerName;
+			this.state.mainTab = 3;
+			if(this.historyEnabled) window.history.pushState(this.state, '');
+			this.trigger(this.state);
 		},
 		popHistoryState: function(event) {
-			state = event.state;
-			this.trigger(state);
+			this.state = event.state;
+			this.trigger(this.state);
 		}
 	});
 })();
